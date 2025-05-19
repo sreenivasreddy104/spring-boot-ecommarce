0493c946629f89e5edf075ef5b020f507633393e
Sree <srinivasareddybandi08@gmail.com>
2025-05-20 01:53:27 +0530

    Code Changes.

--- src/main/java/com/happycoding/ecommarce/repository/ProductRepository.java
+++ src/main/java/com/happycoding/ecommarce/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.happycoding.ecommarce.repository;
 import com.happycoding.ecommarce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(value = "http://localhost:4200")
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
